3875617b6e7620d38eab5b10d5a4719eec06336a
Orkhan Gasimli <orkhan.gasimli@gmail.com>
2018-01-27 15:40:55 +0400

    feat: Update sample app

--- app/src/main/java/org/ogasimli/healthbarview/MainActivity.java
+++ app/src/main/java/org/ogasimli/healthbarview/MainActivity.java
@@ -14,15 +14,6 @@ public class MainActivity extends AppCompatActivity {
 
         HealthBarView view = findViewById(R.id.healthbarview_java);
 
-        // Label
-        view.setShowLabel(true);
-        view.setLabelTextColor(R.color.colorPrimaryDark);
-        view.setLabelTextSize(16);
-        view.setLabelFont(R.font.lato_light_italic);
-        //view.setLabelFont(Typeface.create(Typeface.SERIF, Typeface.ITALIC));
-        view.setLabels(new String[]{"Bad", "Good", "Better", "Best"});
-        //view.setLabels("Bad,Good,Better,Best", Pattern.quote(","));
-
         // Setting min value resets value to min value. Therefore, min value should be set before
         view.setShowMinValue(true);
         view.setMinValue(-20);
@@ -30,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         view.setMinValueTextSize(16);
         view.setMinValueFont(R.font.lato_light_italic);
         //view.setMinValueFont(Typeface.create(Typeface.SERIF, Typeface.ITALIC));
-        view.setMinValueSuffix("%");
+        view.setMinValueSuffix(" pts");
         view.setMinValueDecimalFormat(new DecimalFormat("0.0"));
 
         view.setMaxValue(34);
@@ -39,32 +30,45 @@ public class MainActivity extends AppCompatActivity {
         view.setMaxValueTextSize(16);
         view.setMaxValueFont(R.font.lato_light_italic);
         //view.setMaxValueFont(Typeface.create(Typeface.SERIF, Typeface.ITALIC));
-        view.setMaxValueSuffix("%");
+        view.setMaxValueSuffix(" pts");
         view.setMaxValueDecimalFormat(new DecimalFormat("0.0"));
-
-        // Value
-        view.setShowValue(true);
-        view.setValue(34f);
-        view.setValueTextColor(R.color.colorPrimaryDark);
-        view.setValueTextSize(16);
-        view.setValueFont(R.font.lato_light_italic);
-        //view.setValueFont(Typeface.create(Typeface.SERIF, Typeface.ITALIC));
-        view.setValueSuffix("%");
-        view.setValueDecimalFormat(new DecimalFormat("0.0"));
-        // Animation
-        view.setAnimated(true);
-        view.setAnimationDuration(4000);
-
-        // Bar Indicator
-        view.setIndicatorWidth(0.5f);
-        view.setIndicatorColor(R.color.colorPrimaryDark);
 
         // Bar Stroke
         view.setStrokeWidth(1);
         view.setStrokeColor(R.color.colorPrimaryDark);
 
-        // Bar colors
+        // Bar Fill
         view.setStartColor(R.color.colorPrimary);
         view.setEndColor(R.color.colorAccent);
+
+        // Bar Indicator
+        view.setIndicatorWidth(0.5f);
+        view.setIndicatorColor(R.color.colorPrimaryDark);
+        view.setIndicatorTopOverflow(15);
+        view.setIndicatorBottomOverflow(15);
+
+        // Value
+        view.setShowValue(true);
+        view.setValueTextColor(R.color.colorPrimaryDark);
+        view.setValueTextSize(16);
+        view.setValueFont(R.font.lato_light_italic);
+        //view.setValueFont(Typeface.create(Typeface.SERIF, Typeface.ITALIC));
+        view.setValueSuffix(" pts");
+        view.setValueDecimalFormat(new DecimalFormat("0.0"));
+        // Animation
+        view.setAnimated(true);
+        view.setAnimationDuration(4000);
+        view.setValue(16.1f); // Set value after setting all other attributes of value element
+
+        // Label
+        view.setShowLabel(true);
+        view.setLabelTextColor(R.color.colorPrimaryDark);
+        view.setLabelTextSize(16);
+        view.setLabelFont(R.font.lato_light_italic);
+        //view.setLabelFont(Typeface.create(Typeface.SERIF, Typeface.ITALIC));
+        view.setLabels(new String[]{"Poor","Below Average","Average","Above Average","Good","Excellent"});
+        //view.setLabels("Poor,Below Average,Average,Above Average,Good,Excellent", Pattern.quote(","));
+        view.setLabelsRange(new double[]{-10D,0D,10D,15D,28D,34D});
+        //view.setLabelsRange("-10,0,10,15,28,34", Pattern.quote(","));
     }
 }
